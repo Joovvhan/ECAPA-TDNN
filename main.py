d35faf7771245a947ec628b7e8dabf259c8bbaf2
Joowhan Song <joovvhan.sonq@gmail.com>
2020-12-22 22:06:59 +0900

    Completed building model architecture

--- main.py
+++ main.py
@@ -15,6 +15,41 @@ The performance of this system will be compared to the baselines described in Se
 
 H_CONV = 512
 
+H_ATT = 128
+
+H_FC = 192
+
+NUM_SPEAKERS = 6000
+
+HYPER_RADIUS = 5
+
+AMM_MARGIN = 0.2
+
+class AttentiveStatPooling(nn.Module):
+
+    def __init__(self):
+        super(AttentiveStatPooling, self).__init__()
+        self.linear1 = nn.Linear(3 * H_CONV, H_ATT)
+        self.linear2 = nn.Linear(H_ATT, 3 * H_CONV)   
+
+    def forward(self, input_tensor):
+        h = input_tensor.transpose(1, 2) # (B, H, L) =>  (B, L, H) 
+        tensor = F.relu(self.linear1(h)) # (B, L, 128) 
+        e_tensor = self.linear2(tensor) # (B, L, 1536) 
+        a_tensor = F.softmax(e_tensor, dim=-1) # (B, L, 1536)
+
+        h_mean = torch.sum(torch.mul(h, a_tensor), dim=1, keepdim=False) # (B, H)
+
+        h_square = torch.mul(h, h) # (B, L, H)
+
+        weighted_square = torch.sum(torch.mul(a_tensor, h_square), dim=1)
+
+        sigma = torch.sqrt(weighted_square - torch.mul(h_mean, h_mean)) # (B, H) - (B, H)
+
+        tensor = torch.cat((h_mean, sigma), axis=1)
+
+        return tensor
+
 class SE_RES2Block(nn.Module):
 
     def __init__(self, scale=8, k=3, d=2):
@@ -36,7 +71,6 @@ class SE_RES2Block(nn.Module):
 
         self.fc_1 = nn.Linear(H_CONV, 128)
         self.fc_2 = nn.Linear(128, H_CONV)
-
 
     def forward(self, input_tensor):
 
@@ -98,10 +132,22 @@ class ECAPA_TDNN(nn.Module):
         self.se_res2block_3 = SE_RES2Block(8, 3, 4)
 
         self.conv1d_out = nn.Conv1d(3 * H_CONV, 3 * H_CONV, 1)
-  
 
+        self.attentive_stat_pooling = AttentiveStatPooling()
 
-    def forward(self, input_tensor):
+        self.batchnorm_2 = nn.BatchNorm1d(H_CONV * 3 * 2)
+
+        self.fc = nn.Linear(H_CONV * 3 * 2, H_FC)
+
+        self.batchnorm_3 = nn.BatchNorm1d(H_FC)
+
+        self.speaker_embedding  = nn.utils.weight_norm(nn.Linear(H_FC, NUM_SPEAKERS), dim=0)
+
+        self.scale = HYPER_RADIUS
+        
+        self.m = torch.tensor(AMM_MARGIN, requires_grad=False)
+
+    def forward(self, input_tensor, ground_truth_tensor):
 
         tensor = self.conv1d_in(input_tensor) # (B, M, T) -> (B, C, T)
         tensor = F.relu(tensor)
@@ -114,15 +160,52 @@ class ECAPA_TDNN(nn.Module):
         tensor = torch.cat([tensor_1, tensor_2, tensor_3], axis=1) # (B, 3C, T)
         tensor = F.relu(tensor)
 
-        return tensor
+        '''
+        The dimension of the bottleneck in the SE-Block and the attention module is set to 128.
+        '''
+
+        tensor = self.attentive_stat_pooling(tensor) # (B, H, L) =>  (B, H) 
+
+        tensor = self.batchnorm_2(tensor)
+
+        tensor = self.fc(tensor)
+
+        tensor = self.batchnorm_3(tensor) # (B, H)
+
+        tensor_g = torch.norm(tensor, dim=1, keepdim=True)
+
+        normalized_tensor = tensor / tensor_g
+
+        tensor = self.speaker_embedding(normalized_tensor)
+
+        layer_g = self.speaker_embedding.weight_g
+
+        tensor = tensor / layer_g.squeeze(1)
+
+        # v * torch.cos(self.m) + (1 - v * v) * torch.sin(self.m)
+        # - v * v + v + 1 + torch.cos(self.m) + torch.sin(self.m)
+
+        for i, label in enumerate(ground_truth_tensor):
+            tensor[i, label] = - tensor[i, label] * tensor[i, label] + tensor[i,  label]  + 1 + \
+                                 torch.cos(self.m) + torch.sin(self.m)
+
+        '''
+        All systems are trained using AAM-softmax [6, 25] with a margin of 0.2 and 
+        softmax prescaling of 30 for 4 cycles.
+        '''
+
+        prediction = F.log_softmax(tensor, dim=1) # (B, NUM_SPEAKERS)
+
+        return prediction
 
 def main():
 
     model = ECAPA_TDNN()
 
     input_tensor = torch.rand(B, M, T)
+    ground_truth_tensor = torch.randint(0, T, (B,))
 
-    tensor =  model(input_tensor)
+    tensor = model(input_tensor, ground_truth_tensor)
 
     print(tensor.shape)
     # print(input_tensor.shape)
